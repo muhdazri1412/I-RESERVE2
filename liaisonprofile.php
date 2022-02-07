da4df07d492c203733c3c68dd5edd661802b104b
owish <owishaa@gmail.com>
2022-02-07 20:55:31 +0800

    Update liaisonprofile.php

--- liaisonprofile.php
+++ liaisonprofile.php
@@ -48,6 +48,29 @@ include('connect-db.php');
             }
               </style>
 
+<style>
+
+@media(max-width: 459px){
+.container .content .category{
+  flex-direction: column;
+}
+
+}
+.form-container{
+position: absolute;
+left:15%;
+top:20%;
+max-width: 40%;
+width: 100%;
+background-color: #fff;
+padding: 25px 30px;
+border-radius: 5px;
+
+}
+
+</style>
+
+
 
     </head>
 <BODY class="login">
@@ -118,10 +141,10 @@ if ($result->num_rows > 0) {
        
         
         echo "<div>User Details</div>";
-        echo "<p>Matric   : " .$row['staffid']. "</p>";
+        echo "<p>Staff ID   : " .$row['staffid']. "</p>";
         echo "<p>Username : " .$row['username']. "</p>";
         echo "<p>E-mail   : " .$row['email']. "</p>";
-        echo "<p>Phone    : " .$row['department']. "</p>";
+        echo "<p>Department    : " .$row['department']. "</p>";
        
     }
    
@@ -136,9 +159,15 @@ else {
 
 
 
+
 ?>
 
-    </div>
+    
+
+
+   
+</div>
+
 
 </div>
 
@@ -146,12 +175,51 @@ else {
 
 
 
-
-
-
 </BODY>
 
-<style>
 
-</style>
+<style>
+    .form{
+    margin: 30px;
+    width: 30%;
+    height: 80%;
+    color:#000;
+    position: absolute;
+    bottom: 0px;
+    left: 30%;
+    text-align: left;
+    outline: none;
+    align-items: center;
+}
+
+.form input{
+    width: 80%;
+    background-color: #F1F1F1;
+    border-radius: 5px;
+    border: 0px solid;
+    padding: 12px 20px;
+    margin: 10px 0;
+    text-align: left;
+    }
+.form button {
+        margin: 30px 0;
+        width: 100%;
+        background-color: #677578;
+        border: none;
+        color: black;
+        padding: 10px 100px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        border-radius: 20px;
+        cursor: pointer;
+      }
+
+.already,
+.guest-link{
+    font-size: 12px;
+    color: rgb(0, 153, 255);
+}
+
 </html>
