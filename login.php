ce693fab9991145c81f14b1b2519024c2b6fd68f
owish <owishaa@gmail.com>
2022-02-07 20:55:29 +0800

    Update login.php

--- login.php
+++ login.php
@@ -43,7 +43,7 @@
             <input type="password" placeholder="password" name="password" required><br>
             <button type="submit" class="button">Login</button>
             <a href="loginliaison.php" class="liaison-login"> Login for Liaison officer</a><br>
-            <a href="" class="guest-login"> Login for  IIUM Guest</a><br>
+            <a href="loginguest.php" class="guest-login"> Login for  IIUM Guest</a><br>
             <a href="registration-page.php" class="create-account">Create an account</a><br>
             <a href="password-reset-page.php" class="forgot-password">Forgot password? Start password recovery</a>
           </form>
